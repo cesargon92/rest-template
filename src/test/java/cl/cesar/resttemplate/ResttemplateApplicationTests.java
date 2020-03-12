b62861caf46f1bb000262cd29e8f26d1bc703f74
César González <cesar.gonzalez.992@gmail.com>
2020-03-11 22:01:33 -0300

    first commit

---
+++ src/test/java/cl/cesar/resttemplate/ResttemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.cesar.resttemplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResttemplateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
